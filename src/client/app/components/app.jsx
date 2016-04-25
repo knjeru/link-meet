967c00a5773e1c0fc8329288ed995a8960aec323
Kevin Njeru <knjeru@gmail.com>
2016-04-24 22:45:35 -0600

    Update to Basic build - Added SCSS directory along with Bourbon, Neat, Bitters

--- src/client/app/components/app.jsx
+++ src/client/app/components/app.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
     render(){
         return(
-            <li>Is it working</li>
+            <h2>Meet-Link</h2>
         );
     }
 }
