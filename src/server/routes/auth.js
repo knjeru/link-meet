76b3c8afeb6c453e8848efa8c551dec9eae798f5
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-25 19:03:36 -0600

    Moved helper functions to authHelpers.js and required that file. Added comments

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -2,42 +2,57 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 var request = require('request-promise');
+
+// *** Require Helpers *** //
+var authHelpers = require('./helpers/authHelpers')
+
+// *** Require MongoDB *** //
 var User = require('../models/users');
 var config = require('../../_config');
 
 
-
-
-router.post('/', function(req, res, next) {
-  // ensure user does not already exist
-  User.findOne({email: req.body.email})
-  .
-  then(function (existingUser) {
-    if (existingUser) {
-      return res.status(409).json({ status: 'fail',
-                                    message: 'Email already exists' });
+// Register a new user
+router.post('/register', function(req, res, next) {
+  
+  // ensure the user does not already exist
+  User.findOne( {email: req.body.email} )
+  
+  .then(function (existingUser) {
+    // Return an error message if the email address already exists
+    if (existingUser) { return res.status(409).json({ status: 'fail',
+                                                      message: 'Email already exists' });
     }
     
-    // create new user
+    // If the email address does not exist, add the user to the database
+    // req.body should contain {email, username and password}
     var newUser = new User (req.body);
+    
     newUser.save(function () {
-      // create token
-      var token = generateToken(newUser);
+      // create a jwt token
+      var token = authHelpers.generateToken(newUser);
+      
+      // Send a 'success' status code and message when a new user is added
       res.status(200).json({ status: 'success',
                              data: { token: token,
                                      user: newUser.email }
                           });
     });
+  
   })
   
+  // Return an error if necessary
   .catch(function (err) { return next(err); });
+
 });
 
 
-
+// Login an exisiting user
 router.post('/login', function (req, res, next) {
-  // ensure that user exists
+  
+  // Check for the user using their email address
   User.findOne({email: req.body.email})
+  
+  
   .then(function (user) {
     if (!user) { return res.status(401).json({ status: 'fail',
                                                message: 'Email does not exist' });
@@ -51,70 +66,20 @@ router.post('/login', function (req, res, next) {
       user = user.toObject();
       
       // delete user.password;
-      var token = generateToken(user);
+      var token = authHelpers.generateToken(user);
       res.status(200).json({ status: 'success',
                              data: { token: token,
                                      user: user.email }
       });
+    
     });
+  
   })
   
+  // Return an error if necessary
   .catch(function (err) { return next(err); });
+
 });
 
-
-// ** helpers ** //
-
-// generate a token
-function generateToken(user) {
-  var payload = {
-    exp: moment().add(14, 'days').unix(),
-    iat: moment().unix(),
-    sub: user._id
-  };
-  
-  return jwt.encode(payload, config.TOKEN_SECRET);
-
-}
-
-
-// ensure authenticated
-function ensureAuthenticated(req, res, next) {
-  // check headers for the presence of an auth object
-  if(!(req.headers && req.headers.authorization)) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'No header present or no authorization header.'
-    });
-  }
-  // decode the token
-  var header = req.headers.authorization.split(' ');
-  var token = header[1];
-  var payload = jwt.decode(token, config.TOKEN_SECRET);
-  var now = moment().unix();
-  // ensure that it is valid
-  if(now > payload.exp || payload.iat > now) {
-    return res.status(401).json({
-      status: 'fail',
-      message: 'Token is invalid'
-    });
-  }
-  // ensure user is still in the database
-  User.findById(payload.sub, function(err, user){
-    if(err) {
-      return next(err);
-    }
-    if(!user) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'User does not exist'
-      });
-    }
-    // attach user to request object
-    req.user = user;
-    // call next middleware function
-    next();
-  });
-}
 
 module.exports = router;
