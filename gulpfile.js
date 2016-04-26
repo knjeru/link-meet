b0a73c9da3366fb8395938909c53a8f3db64e252
Kevin Njeru <knjeru@gmail.com>
2016-04-26 11:32:19 -0600

    Added base meetup actions to client-side. updated gulp and package json

--- gulpfile.js
+++ gulpfile.js
@@ -9,30 +9,33 @@ var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
 var connect = require('gulp-connect');
 var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var clean = require('gulp-rimraf');
+var minifyCSS = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
 
+/**
+ * Bourbon & Neat
+ */
+
+var bourbonPaths = require('bourbon').includePaths;
+var neatPaths = require('bourbon-neat').includePaths;
 
 /**
  * Config
  */
 
 var paths = {
-  styles: [
-    './src/client/css/*.css',
-  ],
-  scripts: [
-    './src/client/js/*.js',
-  ],
-  server: [
-    './src/server/bin/www'
-  ],
-  distServer: [
-    './dist/server/bin/www'
-  ]
+  css: './src/client/styles/**/*.css',
+  scss: ['./src/client/styles/scss/*.scss',
+    './src/client/styles/scss/**/*.scss'],
+  scripts: './src/client/js/*.js',
+  server: './src/server/bin/www',
+  distServer: './dist/server/bin/www'
 };
+
+console.log('scss paths', paths.scss);
 
 var nodemonConfig = {
   script: paths.server,
@@ -59,7 +62,7 @@ gulp.task('lint', function() {
 
 gulp.task('browser-sync', ['nodemon'], function(done) {
   browserSync({
-    proxy: "localhost:3000/#/",  // local node app address
+    proxy: "localhost:3000",  // local node app address
     port: 5000,  // use *different* port than above
     notify: true
   }, done);
@@ -81,19 +84,31 @@ gulp.task('nodemon', function (cb) {
       });
 });
 
-gulp.task('watch', function() {
+gulp.task('lint:watch', function() {
   gulp.watch(paths.scripts, ['lint']);
 });
 
 gulp.task('clean', function() {
-  return gulp.src('./dist/*')
-      .pipe(rimraf({force: true}));
+  gulp.src('./dist/*')
+      .pipe(clean({force: true}));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.scss)
+      .pipe(sass({
+        includePaths: ['styles'].concat(bourbonPaths, neatPaths)
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('./src/client/styles/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(paths.scss[1], ['sass']);
 });
 
 gulp.task('minify-css', function() {
-  var opts = {keepSpecialComments:'*'};
-  return gulp.src(paths.styles)
-      .pipe(cleanCSS(opts))
+  var opts = {comments:true, spare:true};
+  gulp.src(paths.styles)
+      .pipe(minifyCSS(opts))
       .pipe(gulp.dest('./dist/client/css/'));
 });
 
@@ -127,12 +142,17 @@ gulp.task('connectDist', function (cb) {
 
 
 // *** default task *** //
-gulp.task('default', ['browser-sync', 'watch'], function(){});
+gulp.task('default', function(){
+  runSequence(
+      ['sass'],
+      ['browser-sync', 'lint:watch', 'sass:watch']
+  );
+});
 
 // *** build task *** //
 gulp.task('build', function() {
   runSequence(
       ['clean'],
-      ['lint', 'clean-css', 'minify-js', 'copy-server-files', 'connectDist']
+      ['lint', 'minify-css', 'minify-js', 'copy-server-files', 'connectDist']
   );
 });
