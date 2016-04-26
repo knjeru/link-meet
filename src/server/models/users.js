f905ad7adce47c5052463377d162e6b7b59f587f
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-25 19:27:35 -0600

    Small tweaks to the styling of the user schema

--- src/server/models/users.js
+++ src/server/models/users.js
@@ -5,31 +5,31 @@ var config = require('../../_config');
 
 
 var UserSchema = new Schema({
-  
+
   username: {
     type: String,
     required: true,
     unique: true
   },
-  
+
   email: {
     type: String,
     required: true,
     unique: true
   },
-  
+
   password: {
     type: String,
     required: true
   },
-  
-  groups : [{ name:     { type: String, 
+
+  groups : [{ name:     { type: String,
                           required: true },
 
-              url:      { type: String, 
+              url:      { type: String,
                           required: true },
-                          
-              photoUrl: { type: String, 
+
+              photoUrl: { type: String,
                           required: true }
            }]
 });
