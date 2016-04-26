53a1105bf9546847ea686c8e181f2ca516f2f6c8
Dennis Bailey <bailey.dennis@gmail.com>
2016-04-25 19:25:58 -0600

    Added comments, status codes, and success messages to these routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,68 +4,62 @@ var http = require('http');
 var rp = require('request-promise');
 
 var apiKey = '475233687d4252104a1a4ff3ae2965';
+var meetupApi = 'https://api.meetup.com';
 
-
-// get all categories
+// Get Meetup categories
 router.get('/categories', function(req, res, next) {
 
-	rp('https://api.meetup.com/2/categories?key=' + apiKey + '&sign=true&photo-host=public&page=40')
-		.then(function(data){
-			var parseData = JSON.parse(data);
-			res.send(parseData.results);
-		})
-		.catch(function(error){
-			return error
-		})
+  // Query the Meetup API to return all available categories
+	rp(meetupApi + '/2/categories?key=' + apiKey + '&sign=true&photo-host=public&page=40')
+  
+  // Return the results and a success message
+  .then(function(data){ res.status(200).json({ status: 'success',
+                                               data: data })
+  })
+		
+  .catch(function(error){ return error })
 
 });
 
-// get all groups in a specific category (by category's id)
+// Get groups with a specific category ID. There are currently 34 categories
 router.get('/categories/:id/groups', function(req, res, next) {
 
-	var id = req.params.id;
-
-	rp('https://api.meetup.com/find/groups?key=' + apiKey + '&sign=true&photo-host=public&category=' + id + '&page=20')
-		.then(function(data){
-			var parseData = JSON.parse(data);
-			res.send(parseData);
-		})
-		.catch(function(error){
-			return error
-		})
+  // Query the Meetup API to return the first 20 groups for a given category
+	rp(meetupApi +'/find/groups?key=' + apiKey + '&sign=true&photo-host=public&category=' + req.params.id + '&page=20')
+	
+	.then(function(data){ res.status(200).json({ status: 'success',
+                                               data: data })
+  })
+	
+	.catch(function(error){ return error })
 
 });
 
-// get all events for a specific group
+// Get events for a specific group. The group name is the `urlname` of the group
 router.get('/groups/:name', function(req, res, next) {
 
-	var name = req.params.name;
-
-	rp('https://api.meetup.com/' + name + '/events?key=' + apiKey + '&sign=true&photo-host=public&page=20')
-		.then(function(data){
-			var parseData = JSON.parse(data);
-			res.send(parseData);
-		})
-		.catch(function(error){
-			return error
-		})
+  // Query the Meetup API to return the first 20 events for a given group name
+	rp(meetupApi + '/' + req.params.name + '/events?key=' + apiKey + '&sign=true&photo-host=public&page=20')
+	
+	.then(function(data){ res.status(200).json({ status: 'success',
+                                               data: data })
+  })
+  	
+	.catch(function(error) { return error });
 
 });
 
-// get RSVP's for an event  
+// Get RSVP's for a given eventID  
 router.get('/groups/:name/events/:event_id', function(req, res, next) {
 
-	var name = req.params.name;
-	var event = req.params.event_id;
-
-	rp('https://api.meetup.com/' + name + '/events/' + event + '/rsvps?' + apiKey + '&sign=true&photo-host=public')
-		.then(function(data){
-			var parseData = JSON.parse(data);
-			res.send(parseData);
-		})
-		.catch(function(error){
-			return error
-		})
+  // Query the Meetup API to return the RSVPs
+	rp('https://api.meetup.com/' + req.params.name + '/events/' + req.params.event_id + '/rsvps?' + apiKey + '&sign=true&photo-host=public')
+	
+	.then(function(data){ res.status(200).json({ status: 'success',
+                                               data: data })
+  })
+	
+	.catch(function(error) { return error })
 
 });
 
