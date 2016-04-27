e0b78e84a2abc50dcace075d0c7ca4c702149196
Lina Sachuk <sachuklina@gmail.com>
2016-04-27 17:38:50 -0600

    css

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -4,7 +4,7 @@ import { Component } from 'react';
 export default class App extends Component {
     render() {
         return (
-            <div>
+            <div className="parent">
                 {this.props.children}
             </div>
         );
