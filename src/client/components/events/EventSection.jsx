502e42f3b767a50664b4aac5df0b30fa6d45f990
Kevin Njeru <knjeru@gmail.com>
2016-04-27 11:54:42 -0600

    We now have a category, group, category component

--- src/client/components/events/EventSection.jsx
+++ src/client/components/events/EventSection.jsx
@@ -1,20 +1,47 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchEvents} from '../../actions/index';
-// import {Link} from 'react-router';
+import {Link} from 'react-router';
+
+
 
 class EventSection extends Component {
+
     componentWillMount() {
-        this.props.fetchEvents(this.props.params.id, this.props.params.name)
+        this.props.fetchEvents(this.props.params.group_name);
     }
 
-  render() {
-      console.log(this.events);
-      return(
-          <div>Events Section</div>
-      );
-  }
+    renderEvents() {
+        return this.props.events.data.map((event) => {
+            return (
+                <li className="list-group-item" key={event.id}>
+                    <Link to={this.props.params.group_name + '/events/' + event.id + '/people'}>
+                        {event.name}
+                    </Link>
+                </li>
+            )
+        })
+    }
+
+    render() {
+        console.log(this.props.events);
+        const {events} = this.props;
+
+        if(!events.data) {
+            return <img src="https://www4.parinc.com/images/loading/loading_bar.gif"/>
+        } else if (events.data) {
+            return (
+                <div>Events
+                    <ul>
+                        {this.renderEvents()}
+                    </ul>
+                </div>
+            );
+        }
+
+    }
 }
+
 
 function mapStateToProps(state) {
     return {events: state.meetUp.events}
