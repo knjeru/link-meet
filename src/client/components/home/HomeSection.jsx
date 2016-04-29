e2a203ce1292937a3986bb82d331d3d542e2baaf
Kevin Njeru <knjeru@gmail.com>
2016-04-29 07:37:04 -0600

    People can now be saved with notes, and a user can view notes in their minders view

--- src/client/components/home/HomeSection.jsx
+++ src/client/components/home/HomeSection.jsx
@@ -4,7 +4,6 @@ class HomeSection extends Component {
     render() {
         return(
             <div className="hero">
-                
                 <h1>Welcome To Meetup Minder</h1>
                 <a href="./login"><button className="mybtn">Login</button></a>
                  <a href="./register"><button className="mybtn">Register</button></a>
--- src/client/components/persons/PersonForm.jsx
+++ src/client/components/persons/PersonForm.jsx
@@ -1,25 +1,81 @@
-// import React, {Component} from 'react';
-// import reduxForm from 'redux-form';
-//
-// class PersonForm extends Component {
-//     render() {
-//         return(
-//             <div>Our Person Form</div>
-//         )
-//     }
-// }
-//
-//
-// function validate(values) {
-//     const errors = {};
-//
-//     return errors;
-// }
-//
-// export default reduxForm(
-//     {
-//         form: 'SavePersonForm',
-//         fields: [''],
-//         validate
-//     }
-// )(PersonForm);
+import React, {Component, PropTypes} from 'react';
+import {reduxForm, initialize} from 'redux-form';
+import {savePerson, fetchPerson} from '../../actions/index';
+
+class PersonForm extends Component {
+
+    componentWillMount() {
+        this.props.fetchPerson(this.props.params.id)
+            .then((data) => {
+                this.props.dispatch(initialize('SavePersonForm', {
+                    name: data.payload.data.data.name,
+                    photoUrl: data.payload.data.data.photo.thumb_link
+                }, ['photoUrl', 'name', 'notes']))
+            });
+    }
+
+    onSubmitForm(props) {
+        this.props.savePerson(localStorage.getItem('id'), props);
+        this.context.router.push('/minders');
+    }
+
+    render() {
+        const {fields: {name, photoUrl, notes}, handleSubmit} = this.props;
+
+        return(
+            <div className="form">
+                <form onSubmit={handleSubmit(this.onSubmitForm.bind(this))}>
+                    <div>
+                        <label className="form-label">Avatar</label>
+                        <input type="text" {...photoUrl}/>
+                    </div>
+                    <div>
+                        <label className="form-label">Name</label>
+                        <input type="text" {...name}/>
+                    </div>
+                    <div>
+                        <label className="form-label">Notes</label>
+                        <textarea type="text" {...notes}/>
+                    </div>
+                    <button type="submit" className="form-btn">Save Person</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+function validate(values) {
+    const errors = {};
+
+    if(!values.name) {
+        errors.name = 'Enter a name!'
+    }
+
+    if(!values.photoUrl) {
+        errors.photoUrl = 'Include a profile picture!'
+    }
+
+    if(!values.notes) {
+        errors.notes = 'Add a note!'
+    }
+
+
+    return errors;
+}
+
+PersonForm.contextTypes = {
+    router: PropTypes.object
+}
+
+function mapStateToProps(state) {
+    return {person: state.meetUp.person}
+}
+
+export default reduxForm
+    ({
+        form: 'SavePersonForm',
+        fields: ['name', 'photoUrl', 'notes'],
+        validate
+    }, mapStateToProps, {savePerson, fetchPerson})
+(PersonForm);
